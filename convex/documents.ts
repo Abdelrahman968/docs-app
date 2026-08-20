44e24509bd95de8e7a971b016ee4ac52f7aa290c
Abdelrahman Ayman <se.abdelrahman968@gmail.com>
2026-08-20 21:44:10 +0300

    fix: fetch documents for authenticated user only

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,11 +1,29 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server";
 import { paginationOptsValidator } from "convex/server";
 
+import { mutation, query } from "./_generated/server";
+
 export const getDocuments = query({
-  args: { paginationOpts: paginationOptsValidator },
+  args: {
+    paginationOpts: paginationOptsValidator,
+  },
+
   handler: async (ctx, args) => {
-    return await ctx.db.query("documents").paginate(args.paginationOpts);
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      return {
+        page: [],
+        isDone: true,
+        continueCursor: "",
+      };
+    }
+
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_owner_id", (q) => q.eq("ownerID", user.subject))
+      .order("desc")
+      .paginate(args.paginationOpts);
   },
 });
 
@@ -14,11 +32,14 @@ export const createDocument = mutation({
     title: v.optional(v.string()),
     initialContent: v.optional(v.string()),
   },
+
   handler: async (ctx, args) => {
     const user = await ctx.auth.getUserIdentity();
+
     if (!user) {
       throw new ConvexError("Not logged in");
     }
+
     return await ctx.db.insert("documents", {
       title: args.title ?? "Untitled Document",
       initialContent: args.initialContent,
